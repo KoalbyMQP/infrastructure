9b0f9a1e413b60f8e5463587fa1f702269fbc8eb
Jonathan_Tinti <jtintinti@gmail.com>
2025-09-09 14:53:16 -0400

    Updated file path for git pull

--- Gideon/scripts/update-on-boot.sh
+++ Gideon/scripts/update-on-boot.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 # Simple log function to pull from git 
 log() { echo -e "[INFO] $(date +'%Y-%m-%d %H:%M:%S') $1"; }
 
-cd /home/lab/NetBootPi || exit 1
+cd /home/lab || exit 1
 
 log "Pulling latest repo..."
 git pull
